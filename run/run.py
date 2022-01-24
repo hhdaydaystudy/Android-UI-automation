df4810543de48db7cdf9861daea7f8c2569a038b
hhdaydaystudy <44293971+hhdaydaystudy@users.noreply.github.com>
2022-01-24 23:33:17 +0800

    Update run.py

--- run/run.py
+++ run/run.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python3
 import sys
-sys.path.append('/Users/lihu/.jenkins/workspace/appium')
+sys.path.append('/Users/lihu/.jenkins/workspace/Android-UI-automation')
 
 import unittest
 import HTMLTestRunner
